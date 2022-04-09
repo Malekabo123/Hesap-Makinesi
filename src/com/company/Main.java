0b0d967b1a1310bfa521565ab1380adc9b2fe568
Malek Alismail <malismail20@posta.pau.edu.tr>
2022-04-10 01:53:59 +0300

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+import java.io.EOFException;
+import java.util.*;
+
+public class Main {
+
+    static void hesapMakiesi() {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter first number...");
+        double a= sc.nextDouble();
+
+        System.out.println("Enter operation...");
+        String b= sc.next();
+
+        System.out.println("Enter second number...");
+        double c= sc.nextDouble();
+
+        if (b.equals("+")){
+            System.out.println(a+c);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        hesapMakiesi();
+    }
+}
